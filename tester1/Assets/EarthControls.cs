01d05951cdb241d1368564b4830826aa3b099294
Vuyi-cpu <vuyim1907@gmail.com>
2025-05-14 20:31:01 +0200

    added triggers
    i added triggers to the black square for earth level

---
+++ tester1/Assets/EarthControls.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EarthControls : MonoBehaviour
+{
+    public int sceneBuildIndex;
+    public void Setup()
+    {
+        gameObject.SetActive(true);
+
+    }
+
+    public void continueButton()
+    {
+        SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+    }
+}
--- tester1/Assets/crossroads/Scripts/MineLevelMove.cs
+++ tester1/Assets/crossroads/Scripts/MineLevelMove.cs
@@ -5,6 +5,7 @@ public class MineLevelMove : MonoBehaviour
     public MineStart MineStart;
     public MineComplete MineComplete;
     public completedGame GameComplete;
+    public staticData3 staticData3;
     public staticData2 staticData2;
     public staticData staticData;
     private void OnTriggerEnter2D(Collider2D other)
@@ -13,7 +14,7 @@ public class MineLevelMove : MonoBehaviour
 
         if (other.tag == "Player")
         {
-            if (staticData.value != true || staticData2.value != true){
+            if (staticData.value != true || staticData2.value != true|| staticData3.value != true){
 
                 if (staticData2.value == false)
                 {
