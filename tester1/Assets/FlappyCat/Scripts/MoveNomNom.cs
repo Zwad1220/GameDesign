807b0427d19d9388ec5e8b3493af284db97e2924
Zwad1220 <khanziyad065@gmail.com>
2025-05-18 11:57:08 +0200

    ligthing (kinda mid rn)

--- tester1/Assets/FlappyCat/Scripts/MoveNomNom.cs
+++ tester1/Assets/FlappyCat/Scripts/MoveNomNom.cs
@@ -1,8 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MoveNomNom : MonoBehaviour
 {
     public float moveSpeed = 5;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
