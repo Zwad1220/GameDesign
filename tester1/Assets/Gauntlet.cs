09fde27ee18d08faf1b14dff572c0431386279ce
Zwad1220 <khanziyad065@gmail.com>
2025-06-17 14:36:56 +0200

    Added gauntlet collected sfx

--- tester1/Assets/Gauntlet.cs
+++ tester1/Assets/Gauntlet.cs
@@ -6,8 +6,10 @@ public class Gauntlet : MonoBehaviour
 {
     public GameObject StoneText;
     public Text Objective;
+    public AudioManager audioManager;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        audioManager.PlaySFX(audioManager.NomNomCollect);
         Destroy(gameObject);
         StoneText.SetActive(true);
         Objective.text = "Objective: Collect all elemental stones.";
